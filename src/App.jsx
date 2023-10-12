b4ce04f4c57743fa136ca57bf7b541de5004fa61
Meisen Shu <65598232+MeisenS@users.noreply.github.com>
2023-10-11 19:23:06 -0500

    add db

--- src/App.jsx
+++ src/App.jsx
@@ -6,15 +6,18 @@ import MenuPage from "./components/MenuPage";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { useJsonQuery } from "./utilities/fetch";
 import Dispatcher from './components/Dispatcher';
+import { DBData } from './utilities/firebase';
 
 
 const Main = () => {
-  const [data, isLoading, error] = useJsonQuery(
-    "https://courses.cs.northwestern.edu/394/guides/data/cs-courses.php"
-  );
+  // const [data, isLoading, error] = useJsonQuery(
+  //   "https://courses.cs.northwestern.edu/394/guides/data/cs-courses.php"
+  // );
+
+  const [data, error] = DBData('/');
 
   if (error) return <h1>Error loading user data: {`${error}`}</h1>;
-  if (isLoading) return <h1>Loading user data...</h1>;
+  // if (isLoading) return <h1>Loading user data...</h1>;
   if (!data) return <h1>No user data found</h1>;
   // console.log(data.courses);
 
--- src/utilities/firebase.js
+++ src/utilities/firebase.js
@@ -1,19 +1,43 @@
-import { getAuth, GoogleAuthProvider, onAuthStateChanged, signInWithPopup, signOut } from 'firebase/auth';
 
-export const signInWithGoogle = () => {
-    signInWithPopup(getAuth(firebase), new GoogleAuthProvider());
+import { initializeApp } from 'firebase/app';
+import { getDatabase, onValue, ref, update} from 'firebase/database';
+import { useCallback, useEffect, useState } from 'react';
+
+const firebaseConfig = {
+    databaseURL: "https://project-38a8e-default-rtdb.firebaseio.com/",
 };
 
-const firebaseSignOut = () => signOut(getAuth(firebase));
+const firebase = initializeApp(firebaseConfig);
+const database = getDatabase(firebase);
 
-export { firebaseSignOut as signOut };
+export const DBData = (path) => {
+  const [data, setData] = useState();
+  const [error, setError] = useState(null);
 
-export const useAuthState = () => {
-    const [user, setUser] = useState();
+  useEffect(() => (
+    onValue(ref(database, path), (snapshot) => {
+     setData( snapshot.val() );
+    }, (error) => {
+      setError(error);
+    })
+  ), [ path ]);
 
-    useEffect(() => (
-        onAuthStateChanged(getAuth(firebase), setUser)
-    ), []);
+  return [ data, error ];
+};
 
-    return [user];
+const makeResult = (error) => {
+  const timestamp = Date.now();
+  const message = error?.message || `Updated: ${new Date(timestamp).toLocaleString()}`;
+  return { timestamp, error, message };
+};
+
+export const DatabaseUpdate = (path) => {
+  const [result, setResult] = useState();
+  const updateData = useCallback((value) => {
+    update(ref(database, path), value)
+    .then(() => setResult(makeResult()))
+    .catch((error) => setResult(makeResult(error)))
+  }, [database, path]);
+
+  return [updateData, result];
 };
